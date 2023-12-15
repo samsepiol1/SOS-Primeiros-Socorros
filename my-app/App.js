a665bdf9e071b980bbb99da5df010d7b9644fab2
Lucas Matheus <49918079+samsepiol1@users.noreply.github.com>
2023-12-15 16:10:59 +0000

    adicionando plano de testes em md

--- my-app/App.js
+++ my-app/App.js
@@ -4,8 +4,11 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './HomeScreen';
 import DonationScreen from './DonationScreen';
-import UrgencyScreen from './UrgencyScreen'; // Adicione a nova tela aqui
-
+import UrgencyScreen from './UrgencyScreen'; 
+import BurningScreen from './BurningScreen';
+import CutScreen from './CutScreen';
+import DrowningScreen from './DrowningScreen';
+import ChokingScreen from './ChokingScreen';
 const Stack = createStackNavigator();
 
 const App = () => {
@@ -15,6 +18,11 @@ const App = () => {
         <Stack.Screen name="S.O.S - Primeiros Socorros" component={HomeScreen} />
         <Stack.Screen name="Donation" component={DonationScreen} />
         <Stack.Screen name="Urgency" component={UrgencyScreen} />
+        <Stack.Screen name="Burning" component={BurningScreen} />
+        <Stack.Screen name="Cutting" component={CutScreen} />
+        <Stack.Screen name="Drown" component={DrowningScreen} />
+        <Stack.Screen name="Choke" component={ChokingScreen} />
+
         
       </Stack.Navigator>
     </NavigationContainer>
